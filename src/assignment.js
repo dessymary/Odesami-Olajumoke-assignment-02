cdc7f0dc414304cbbc3df2b0d6b6652114edb50e
Odesanmi Olajumoke Mary <106171151+dessymary@users.noreply.github.com>
2022-07-14 13:12:37 +0000

    check challenge one and two

--- src/assignment.js
+++ src/assignment.js
@@ -4,19 +4,26 @@ const assignment = {};
 // ========================
 
 
-/**
- * Challenge - 1
- * 
- * Calculate and return the sum of the numbers in an array.
- * If you did Challenge - 1, remove the comment in the line just after this function
- * 
- * @param {Array} arrayOfNumbers the array of numbers to sum
- * @returns number the sum of the numbers
- */
+
+ // * Challenge - 1
+ // * 
+ // * Calculate and return the sum of the numbers in an array.
+ // * If you did Challenge - 1, remove the comment in the line just after this function
+ // * 
+ // * @param {Array} arrayOfNumbers the array of numbers to sum
+ // * @returns number the sum of the numbers
+
+
 function sumOfNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
-}
-// assignment.sumOfNumbers = sumOfNumbers;
+  let sum= 0;
+  for (let i= 0; i< arrayOfNumbers.length; i++) {
+    sum= sum + arrayOfNumbers[i];
+  }
+  return sum;
+}  
+
+sumOfNumbers([2,3,98,30,33,4,1,7]);
+assignment.sumOfNumbers = sumOfNumbers;
 
 /**
  * Challenge - 2
@@ -27,10 +34,18 @@ function sumOfNumbers(arrayOfNumbers) {
  * @param {Array} arrayOfNumbers the array containing even or non-even numbers
  * @returns number the count of even numbers
  */
-function countEvenNumbers(arrayOfNumbers) {
-    return arrayOfNumbers.length;
+
+function countEvenNumbers(arr) {
+  let counter=0;
+  for(let i= 0; i< arr.length; i++) {
+      if ( arr[i] % 2 === 0) {
+        counter++;
+      }
+  }
+    return counter;
 }
-// assignment.countEvenNumbers = countEvenNumbers;
+ countEvenNumbers([2,5,6,8,9,8]);
+ assignment.countEvenNumbers = countEvenNumbers;
 
 /**
  * Challenge - 3
